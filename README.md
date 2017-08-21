0b30a8f64540b04e0cbfc137613acccdfa37fe25
Cory Ginsberg <cory@coryginsberg.com>
2017-08-21 10:10:15 -0500

    Add a timeout of 1 second. Add a 'Done' alert.

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 #Google Webmaster Tools Bulk Url Removal Chrome Extension
 ##Installation 
-1.  Download the extension from [github](https://github.com/noitcudni/google-webmaster-tools-bulk-url-removal/archive/master.zip) and unzip it.
+1.  Download the extension from [github](https://github.com/coryginsberg/google-webmaster-tools-bulk-url-removal/archive/master.zip) and unzip it.
 2. Go to **chrome://extensions/** and turn on Developer mode.
 3. Click on **Load unpacked extension . . .** and load the extension.
 
 ##Usage
-1. Create a list of urls to be removed and store them in a file. All urls are separated by \n. See [How to Extract a List of All URLs Indexed by Google for Your Website](https://www.highposition.com/blog/how-to-extract-list-urls-indexed-by-google-for-your-website/) for an easy way to get all links from a website from Google Search results.
+1. Create a list of urls to be removed and store them in a file. All urls are separated by `\n`. See [How to Extract a List of All URLs Indexed by Google for Your Website](https://www.highposition.com/blog/how-to-extract-list-urls-indexed-by-google-for-your-website/) for an easy way to get all links from a website from Google Search results.
 2. Go to Google's webmaster tools.
-3. Click on Optimization -> Remove URLs.
+3. Click on Google Index -> Remove URLs.
 4. You should now see a new drop-down with several removal options, along with a "Choose File" button.
 5. Click on the Choose File button.
 6. Select the file you created in step 3.
+7. The URLs should automatically begin being removed. Be patient. This can take a while depending on the number of URLs.
--- bg.js
+++ bg.js
@@ -3,57 +3,46 @@ var removalMethod = null;
 var victimUrlArray = null;
 
 chrome.runtime.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-    if (msg.type === 'initVictims') {
-      executionInProgress = true;
-      victimUrlArray = msg.rawTxt.replace(/^\s+|\s+$/g, '').split('\n');
-      removalMethod = msg.removalMethod;
+    port.onMessage.addListener(function(msg) {
+        if (msg.type === 'initVictims') {
+            executionInProgress = true;
+            victimUrlArray = msg.rawTxt.replace(/^\s+|\s+$/g, '').split('\n');
+            removalMethod = msg.removalMethod;
 
-      var victimUrl = victimUrlArray.pop();
-      port.postMessage({
-        'type' : 'removeUrl',
-        'victim': victimUrl,
-        'removalMethod': removalMethod
-      });
-    } else if (msg.type === 'nextVictim') {
-      // find the next victim
-      if (executionInProgress) {
-        var victimUrl = victimUrlArray.pop();
-        if (victimUrl !== undefined) {
-          port.postMessage({
-            'type' : 'removeUrl',
-            'victim': victimUrl,
-            'removalMethod': removalMethod
-          });
-        } else {
-          executionInProgress = false; //done
-          removalMethod = null;
-          victimUrlArray = null;
+            var victimUrl = victimUrlArray.pop();
+            port.postMessage({
+                'type': 'removeUrl',
+                'victim': victimUrl,
+                'removalMethod': removalMethod
+            });
+        } else if (msg.type === 'nextVictim') {
+            // find the next victim
+            if (executionInProgress) {
+                // Set a 1 second timeout between requests as Google will prevent multiple requests coming in too fast
+                setTimeout(function() {
+                    var victimUrl = victimUrlArray.pop();
+                    if (victimUrl !== undefined) {
+                        port.postMessage({
+                            'type': 'removeUrl',
+                            'victim': victimUrl,
+                            'removalMethod': removalMethod
+                        });
+                    } else {
+                        executionInProgress = false; //done
+                        removalMethod = null;
+                        victimUrlArray = null;
+                        alert("Done!");
+                    }
+                }, 1000); // 1 Second timeout
+            } else {
+                console.log("no victim to be executed."); //xxx
+            }
+        } else if (msg.type == 'askState') {
+            port.postMessage({
+                'type': 'state',
+                'executionInProgress': executionInProgress,
+                'removalMethod': removalMethod
+            });
         }
-      } else {
-        console.log("no victim to be executed."); //xxx
-      }
-    } else if (msg.type == 'askState') {
-      port.postMessage({
-        'type' : 'state',
-        'executionInProgress' : executionInProgress,
-        'removalMethod' : removalMethod
-      });
-   }
-  });
+    });
 });
-
-
-//chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  //if (executionInProgress) {
-    //chrome.tabs.getSelected(null,function(tab){
-        //myURL=tab.url;
-        //console.log(myURL); //xxx
-    //});
-    ////if (changeInfo.url.match(//)) {
-
-    ////}
-    //console.log("should be clicking on some button to close the deal");
-
-  //}
-//});
